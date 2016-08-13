3e2fc4dff06ecd7e942e1e3f3306bd43a6510197
James Michiemo <james.michiemo@gmail.com>
2016-08-12 22:55:26 -0400

    Add Cutestrap styling

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,14 +2,14 @@ class PiecesController < ApplicationController
   # GET /pieces
   # GET /pieces.json
   def index
-    @video_stream = VideoStream.first.url
+    @video_stream = VideoStream.first
     @pieces = Piece.published.order(:created_at).page(params[:page])
   end
 
   # GET /pieces/1
   # GET /pieces/1.json
   def show
-    @video_stream = VideoStream.first.url
+    @video_stream = VideoStream.first
     @piece = Piece.published.find(params[:id])
   end
 end
