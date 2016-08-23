d06624ccc6710f43b032b51438f92f8ac7186b33
James Michiemo <james.michiemo@gmail.com>
2016-08-23 18:11:53 -0400

    Replace Photo Migration

---
+++ db/migrate/20160823220608_remove_photo_from_pieces.rb
@@ -0,0 +1,5 @@
+class RemovePhotoFromPieces < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :pieces, :photo, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818142342) do
+ActiveRecord::Schema.define(version: 20160823220608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20160818142342) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pieces", force: :cascade do |t|
-    t.string   "photo"
     t.string   "video"
     t.string   "audio"
     t.text     "words"
