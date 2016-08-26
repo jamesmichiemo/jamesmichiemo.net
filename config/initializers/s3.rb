2fa9a6960e125b7d971dbe6ba472a8d46b324952
James Michiemo <james.michiemo@gmail.com>
2016-08-26 16:42:09 -0400

    Add S3 region to config

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['SIMPLE_KEY'] || "foo",
-    :aws_secret_access_key  => ENV['SIMPLE_SECRET'] || "bar"
+    :aws_secret_access_key  => ENV['SIMPLE_SECRET'] || "bar",
+    :region                 => 'us-west-2'
   }
   config.fog_directory  = ENV['SIMPLE_BUCKET'] || "replace-me"
   config.fog_use_ssl_for_aws = true
