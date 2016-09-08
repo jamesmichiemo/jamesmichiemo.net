c76361370a17d4455de98116c9f2db5d56a21d31
James Michiemo <james.michiemo@gmail.com>
2016-09-08 17:25:21 -0400

    Progressive Enhancement

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,150 +8,150 @@
 #
 require 'securerandom'
 
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :words => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :words => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :caption => SecureRandom.hex(6),
-    :state => 'unpublished'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :caption => SecureRandom.hex(6),
+#     :state => 'unpublished'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :words => SecureRandom.hex(6),
-    :caption => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :words => SecureRandom.hex(6),
+#     :caption => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :words => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
-
-
-
-5.times do
-  Piece.create(
-    :caption => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
-
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
-
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
-
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :words => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
-
-5.times do
-  Piece.create(
-    :video => SecureRandom.hex(6),
-    :caption => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :words => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
 
-5.times do
-  Piece.create(
-    :state => 'published'
-  )
-end
 
-5.times do
-  Piece.create(
-    :words => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :caption => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :caption => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :words => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :words => SecureRandom.hex(6),
-    :caption => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :words => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-5.times do
-  Piece.create(
-    :caption => SecureRandom.hex(6),
-    :state => 'published'
-  )
-end
+# 5.times do
+#   Piece.create(
+#     :video => SecureRandom.hex(6),
+#     :caption => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
 
-VideoStream.create(url:'http://player.twitch.tv/?channel=shaeta')
+
+# 5.times do
+#   Piece.create(
+#     :state => 'published'
+#   )
+# end
+
+# 5.times do
+#   Piece.create(
+#     :words => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
+
+# 5.times do
+#   Piece.create(
+#     :caption => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
+
+# 5.times do
+#   Piece.create(
+#     :words => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
+
+# 5.times do
+#   Piece.create(
+#     :words => SecureRandom.hex(6),
+#     :caption => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
+
+# 5.times do
+#   Piece.create(
+#     :caption => SecureRandom.hex(6),
+#     :state => 'published'
+#   )
+# end
+
+# VideoStream.create(url:'http://player.twitch.tv/?channel=shaeta')
 
 User.create(email:'admin@toyan.com', password:'secret', password_confirmation:'secret')
