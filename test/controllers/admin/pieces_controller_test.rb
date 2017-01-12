9d9461d856c9850cd267066066e262c8969e62c6
James Michiemo <james.michiemo@gmail.com>
2017-01-12 07:20:18 -0500

    Validation!

---
+++ test/controllers/admin/pieces_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class Admin::PiecesControllerTest < ActionDispatch::IntegrationTest
+  # setup do
+  #   @piece = pieces(:one)
+  # end
+
+  # test "should get index" do
+  #   get admin_pieces_path
+  #   assert_response :success
+  # end
+
+  # test "should get new" do
+  #   get new_admin_piece_url
+  #   assert_response :success
+  # end
+
+  # test "should create piece" do
+  #   assert_difference('Piece.count') do
+  #     post admin_piece_url, params: { piece: { audio: @piece.audio, caption: @piece.caption, video: @piece.video, words: @piece.words } }
+  #   end
+
+  #   assert_redirected_to admin_piece_url(Piece.last)
+  # end
+
+  # test "should show piece" do
+  #   get admin_piece_url(@piece)
+  #   assert_response :success
+  # end
+
+  # test "should get edit" do
+  #   get edit_admin_piece_url(@piece)
+  #   assert_response :success
+  # end
+
+  # test "should update piece" do
+  #   patch admin_piece_url(@piece), params: { piece: { audio: @piece.audio, caption: @piece.caption, video: @piece.video, words: @piece.words } }
+  #   assert_redirected_to admin_piece_url(@piece)
+  # end
+
+  # test "should destroy piece" do
+  #   assert_difference('Piece.count', -1) do
+  #     delete admin_piece_url(@piece)
+  #   end
+
+  #   assert_redirected_to admin_pieces_url
+  # end
+end
